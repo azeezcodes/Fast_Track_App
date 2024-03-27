a4fd3eb328acf7d0bbd0f187a0f1de8a71c9a683
azeezcodes <81769914+azeezcodes@users.noreply.github.com>
2024-03-27 09:47:39 +0100

    adjusted the env file

--- firebaseConfig.jsx
+++ firebaseConfig.jsx
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-    apiKey: "AIzaSyDXjidevJE_Z9ZTwXCzAdH_c42dsB6J-Y0",
-    authDomain: "fastmarket-8051a.firebaseapp.com",
-    projectId: "fastmarket-8051a",
-    storageBucket: "fastmarket-8051a.appspot.com",
-    messagingSenderId: "69885652061",
-    appId: "1:69885652061:web:ec4bb9f26f56f99b0171b4"
+    apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+    authDomain: process.env.REACT_APP_authDomain,
+    projectId: process.env.REACT_APP_projectId,
+    storageBucket: process.env.REACT_APP_storageBucket,
+    messagingSenderId: process.env.REACT_APP_messagingSenderId,
+    appId: process.env.REACT_APP_appId,
 };
 
 export const app = initializeApp(firebaseConfig);
